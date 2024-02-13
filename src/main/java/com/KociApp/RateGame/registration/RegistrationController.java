06d9a1b7a15131b8630fde5f5e6b0e1899f56832
Lukasz <lukasz,kocurek@edu.uekat.pl>
2024-02-13 13:08:05 +0100

    Added event RegistrationCompleteEvent, and made confirmation link creation method in RegistrationController

--- src/main/java/com/KociApp/RateGame/registration/RegistrationController.java
+++ src/main/java/com/KociApp/RateGame/registration/RegistrationController.java
@@ -1,8 +1,11 @@
 package com.KociApp.RateGame.registration;
 
+import com.KociApp.RateGame.event.RegistrationCompleteEvent;
 import com.KociApp.RateGame.user.User;
 import com.KociApp.RateGame.user.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class RegistrationController {
 
     private final UserService userService;
+    private final ApplicationEventPublisher publisher;
 
     @PostMapping
-    public String registerUser(@RequestBody RegistrationRequest request){
+    public String registerUser(RegistrationRequest request, final HttpServletRequest httpRequest){
 
         User user = userService.userRegistration(request);
+        publisher.publishEvent(new RegistrationCompleteEvent(user, applicationURL(httpRequest)));
 
         return "User " + user.getUsername() + " succesfully registered, click on the link in the email we sent you!";
+    }
+
+    private String applicationURL(HttpServletRequest httpRequest) {
+
+        return "http://" + httpRequest.getServerName() + ":" + httpRequest.getServerPort() + httpRequest.getContextPath();
     }
 }
