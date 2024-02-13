822679a505c8c2f3a784cc2b737b40df1475a606
Lukasz <lukasz,kocurek@edu.uekat.pl>
2024-02-13 09:39:18 +0100

    Added JPARepository for UserService

--- src/main/java/com/KociApp/RateGame/user/UserService.java
+++ src/main/java/com/KociApp/RateGame/user/UserService.java
@@ -1,11 +1,17 @@
 package com.KociApp.RateGame.user;
 
 import com.KociApp.RateGame.registration.RegistrationRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
+@AllArgsConstructor
 public class UserService implements IUserService{
+
+    private final UserRepository repository;
     @Override
     public List<User> getUsers() {
         return null;
