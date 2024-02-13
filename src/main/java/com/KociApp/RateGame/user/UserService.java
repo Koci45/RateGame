5108d8540cb761537f58cb4b1b2f30fc80535330
Lukasz <lukasz,kocurek@edu.uekat.pl>
2024-02-13 10:21:36 +0100

    Added user registration method in UserService

--- src/main/java/com/KociApp/RateGame/user/UserService.java
+++ src/main/java/com/KociApp/RateGame/user/UserService.java
@@ -1,7 +1,9 @@
 package com.KociApp.RateGame.user;
 
+import com.KociApp.RateGame.exception.UserAlreadyExistsException;
 import com.KociApp.RateGame.registration.RegistrationRequest;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,18 +14,30 @@ import java.util.Optional;
 public class UserService implements IUserService{
 
     private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
     @Override
     public List<User> getUsers() {
-        return null;
+        return repository.findAll();
     }
 
     @Override
     public User userRegistration(RegistrationRequest request) {
-        return null;
+
+        Optional<User> user = repository.findByEmail(request.email());
+        if(user.isPresent()){
+            throw new UserAlreadyExistsException("User with email - " + request.email() + "already exists");
+        }
+
+        var newUser = new User();
+        newUser.setUsername(request.username());
+        newUser.setPassword(passwordEncoder.encode(request.password()));
+        newUser.setRole(request.role());
+        newUser.setEmail(request.email());
+        return repository.save(newUser);
     }
 
     @Override
     public Optional<User> findByEmail(String email) {
-        return Optional.empty();
+        return repository.findByEmail(email);
     }
 }
